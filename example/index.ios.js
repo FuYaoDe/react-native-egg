7f9a001767e0fecb89c288e1080a626748a59384
FuYaoDe <dan82625@gmail.com>
2016-05-24 16:56:00 +0800

    add Egg.js

--- example/index.ios.js
+++ example/index.ios.js
@@ -1,17 +1,17 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Text,
   View,
-  PanResponder,
+  ScrollView,
 } from 'react-native';
-
+import Egg from './Egg';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,86 +19,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
 
 class example extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      setp: [],
-      num: -1,
-    };
-  }
-
-  componentWillMount() {
-    this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-      onPanResponderRelease: (evt, gestureState) => {
-        this.gestureSetp(gestureState);
-      },
-      onShouldBlockNativeResponder: (evt, gestureState) => {
-        return true;
-      },
-    });
-  }
-
-  between = (input, min, max) => {
-    return input >= min && input <= max;
-  }
-
-  gestureSetp = (gestureState) => {
-    // console.log("onPanResponderRelease", JSON.stringify(gestureState, null, 2));
-    const { dx, dy } = gestureState;
-    const isXMove = !this.between(0, dx - 15, dx + 15) && Math.abs(dx) > Math.abs(dy);
-    const isYMove = !this.between(0, dy - 15, dy + 15) && Math.abs(dy) > Math.abs(dx);
-    const isTap = !isXMove && !isYMove;
-    let setp;
-    if (isXMove) {
-      if (dx > 0) {
-        setp = 'R';
-      } else {
-        setp = 'L';
-      }
-    } else if (isYMove) {
-      if (dy > 0) {
-        setp = 'D';
-      } else {
-        setp = 'U';
-      }
-    } else if (isTap) {
-      setp = 'T';
-    }
-
-    let newSetp = [...this.state.setp];
-    newSetp[(this.state.num + 1) % 30] = setp;
-    this.setState({
-      setp: newSetp,
-      num: this.state.num + 1,
-    });
-
-    const find = ',';
-    const re = new RegExp(find, 'g');
-    let setpString = newSetp.toString().replace(re, '');
-
-    setpString = newSetp.length >= 30 ? setpString + setpString : setpString;
-    console.log(setp, setpString);
-    if ( setpString.indexOf("UUDDLRLR") !== -1) {
-      console.log("!!!!!!!!!!!");
-      this.setState({
-        setp: [],
-        num: -1,
-      });
-    }
-  }
-
-
   render() {
     return (
-      <View {...this._panResponder.panHandlers} style={styles.container} />
+      <Egg style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+      </Egg>
     );
   }
 }
-
 
 AppRegistry.registerComponent('example', () => example);
