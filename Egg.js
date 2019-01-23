4dc4547af38d388e4c80650a6d1eed18720a3438
Jens Kuhr Jørgensen <jens@journl.dk>
2019-01-23 14:31:46 +0100

    Allow interaction through egg, iOS
    Allow interaction with touchable components inside egg on iOS. I experienced problems with text inputs in a webview that were hard to focus when the webview was put inside egg. This fix doesn't really help on Android.

--- Egg.js
+++ Egg.js
@@ -20,7 +20,7 @@ export default class Egg extends Component {
   componentWillMount() {
     this._panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => false,
       onPanResponderGrant: (evt, gestureState) => {
         if (this.props.touchOpacity) {
           Animated.timing(
